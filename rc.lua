3bba41c4a315084da6a5675ed525c7c019654c33
David Kosorin <david@kosorin.net>
2023-02-28 15:14:25 +0100

    Update project files

--- rc.lua
+++ rc.lua
@@ -15,5 +15,7 @@ require("core")
 require("services")
 require("ui")
 
+---@diagnostic disable: param-type-mismatch
 collectgarbage("setpause", 110)
 collectgarbage("setstepmul", 1000)
+---@diagnostic enable: param-type-mismatch
